7dca2e64f8465d7f13e46ef6df79491ae25b7a8e
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-06-07 00:22:53 -0700

    Trying exercise 106 again

---
+++ basicpythonexercise.py
@@ -0,0 +1,15 @@
+x = 2 ** (.5)
+y = 1 + (1/3.0)
+x_diff = 0
+y_diff = 0
+
+if (x == y):
+    print ("x = ", x)
+elif (x > y):
+    x_diff = x-y
+    print ("x_diff = " , x_diff)
+else:
+    y_diff = x-y
+    print ("y_diff = " , y_diff)
+
+    
