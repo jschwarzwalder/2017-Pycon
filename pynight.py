68bf6eb2ee0412ef6ff78812bfe2fb998c413bc6
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-06-07 00:22:31 -0700

    Challenge 106

---
+++ pynight.py
@@ -0,0 +1,18 @@
+# add
+(((10 + 2) * 5) - ((5 * 10) + 2))
+
+#Diff
+x = 2**(1/2.0)
+y = 1 + (1/3.0)
+x_diff = 0
+y_diff = 0
+
+if (x == y):
+    print(x)
+elif (x > y):
+    x_diff = x - y 
+elif (x < y) :
+    y_diff = y - x
+    
+print (x_diff)
+print (y_diff)
