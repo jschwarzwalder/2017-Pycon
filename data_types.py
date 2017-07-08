14e9ec9eca3448a7979131e1e070731b14b411e8
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-07-07 23:51:45 -0700

    process inputs and cast to data type

---
+++ data_types.py
@@ -0,0 +1,20 @@
+
+# Declare second integer, double, and String variables.
+integer = 3
+double = 3.0
+string = "three"
+
+# Read and save an integer, double, and String to your variables.
+new_int = integer + int(input())
+new_double = double + float(input())
+new_string = string + input()
+
+# Print the sum of both integer variables on a new line.
+print(new_int)
+
+# Print the sum of the double variables on a new line.
+print(new_double)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(new_string)
